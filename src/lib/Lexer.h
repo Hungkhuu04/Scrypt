323f062ccd6ef5578c76020e8b81a531f6994a28
Hung Khuu <hungkhuu@ucsb.edu>
2023-10-11 22:25:54 -0700

    Lexer

---
+++ src/lib/Lexer.h
@@ -0,0 +1,28 @@
+<<<<<<< Updated upstream
+=======
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Token.h"
+
+class Lexer {
+public:
+    Lexer(const std::string& input);
+    std::vector<Token> tokenize();
+
+private:
+    char peek();
+    char consume();
+    bool isDigit(char c);
+    bool isOperator(char c);
+    Token number();
+    Token op();
+
+    std::string input;
+    int length;
+    int pos;
+    int line;
+    int col;
+};
+>>>>>>> Stashed changes
