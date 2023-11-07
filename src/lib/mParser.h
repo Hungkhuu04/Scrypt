f948ae1b62ef73976dcd0fc4c731df262a038753
Hung Khuu <hungkhuu@ucsb.edu>
2023-11-06 16:28:41 -0800

    change node name

--- src/lib/mParser.h
+++ src/lib/mParser.h
@@ -7,8 +7,8 @@
 #include <iostream>
 #include<map>
 
-// Enum representing different node types in the AST
-enum class NodeType {
+// Enum representing different mnode types in the AST
+enum class mNodeType {
     ADD, SUBTRACT, MULTIPLY, DIVIDE, NUMBER, IDENTIFIER, ASSIGN,
     BOOLEAN, LESS_THAN, LESS_EQUAL, GREATER_THAN, GREATER_EQUAL,
     EQUAL, NOT_EQUAL, LOGICAL_AND, LOGICAL_XOR, LOGICAL_OR, 
@@ -22,14 +22,14 @@ enum class NodeType {
     NEWLINE,
 };
 
-// Structure representing a node in the AST
-struct Node {
-    NodeType type;
+// Structure representing a mnode in the AST
+struct mNode {
+    mNodeType type;
     double value;
     bool boolValue;
     std::string identifier;
-    std::vector<Node*> children;
-    Node(NodeType t, double v = 0, const std::string& id = "") 
+    std::vector<mNode*> children;
+    mNode(mNodeType t, double v = 0, const std::string& id = "") 
         : type(t), value(v), identifier(id) {}
 };
 
@@ -38,34 +38,34 @@ class mParser {
 private:
     std::vector<Token> tokens;
     int currentTokenIndex;
-    Node* root;
+    mNode* root;
     int unmatchedParentheses = 0;
 
-    Node*assignmentExpression(std::ostream& os);
-    Node* logicalOrExpression(std::ostream& os);
-    Node* logicalAndExpression(std::ostream& os);
-    Node* equalityExpression(std::ostream& os);
-    Node* relationalExpression(std::ostream& os);
-    Node* additiveExpression(std::ostream& os); 
-    Node* multiplicativeExpression(std::ostream& os);
-    Node* logicalXorExpression(std::ostream& os);
-    Node* expression(std::ostream& os = std::cerr); //Arithmetic expression like + or -
+    mNode*assignmentExpression(std::ostream& os);
+    mNode* logicalOrExpression(std::ostream& os);
+    mNode* logicalAndExpression(std::ostream& os);
+    mNode* equalityExpression(std::ostream& os);
+    mNode* relationalExpression(std::ostream& os);
+    mNode* additiveExpression(std::ostream& os); 
+    mNode* multiplicativeExpression(std::ostream& os);
+    mNode* logicalXorExpression(std::ostream& os);
+    mNode* expression(std::ostream& os = std::cerr); //Arithmetic expression like + or -
 
     Token& currentToken();
-    Node* factor(std::ostream& os = std::cerr); // The most basic element whether it is an expression in parenthesis or numbers or variables.
+    mNode* factor(std::ostream& os = std::cerr); // The most basic element whether it is an expression in parenthesis or numbers or variables.
 
-    Node* ifStatement(std::ostream& os);
-    Node* whileStatement(std::ostream& os);
-    Node* statement(std::ostream& os);
-    Node* printStatement(std::ostream& os);
-    Node* assignmentStatement(std::ostream& os);
-    Node* parseBlock(std::ostream& os);
+    mNode* ifStatement(std::ostream& os);
+    mNode* whileStatement(std::ostream& os);
+    mNode* statement(std::ostream& os);
+    mNode* printStatement(std::ostream& os);
+    mNode* assignmentStatement(std::ostream& os);
+    mNode* parseBlock(std::ostream& os);
 
 public:
     mParser(const std::vector<Token>& tokens);
     ~mParser();
-    Node* parse(std::ostream& os);
-    void clearTree(Node*& node);
+    mNode* parse(std::ostream& os);
+    void clearTree(mNode*& mnode);
 };
 
 #endif
